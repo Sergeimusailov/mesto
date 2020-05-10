c4d827c539195e70e98309662ac47edc84d6f3df
Sergei Musailov <sergeymusailov@yandex.ru>
2020-05-10 16:29:06 +0300

    feat: добавить отправку данных в форме

--- script/script.js
+++ script/script.js
@@ -1,11 +1,31 @@
-let buttonEdit = document.querySelector('.profile__button_edit');
-let popup = document.querySelector('.popup');
-let buttonClose = document.querySelector('.popup__close-icon');
+// Сценарий открытия/закрытия попапа
+const buttonEdit = document.querySelector('.profile__button_edit');
+const popup = document.querySelector('.popup');
+const buttonClose = document.querySelector('.popup__close-icon');
 
 buttonEdit.addEventListener('click', function() {
   popup.classList.add('popup_opened');
 });
 
-buttonClose.addEventListener('click', function() {
+function popupClose() {
   popup.classList.remove('popup_opened');
-});
+}
+
+buttonClose.addEventListener('click', popupClose);
+
+// Отправка данных в форме
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+let formElement = document.querySelector('.popup__form');
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    let nameInput = document.querySelector('#heading');
+    let jobInput = document.querySelector('#subheading');
+
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+};
+formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', popupClose);
