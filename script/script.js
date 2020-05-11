54b047b9792e730bb944a1eb8737a1978f3077dc
Sergei Musailov <sergeymusailov@yandex.ru>
2020-05-11 19:07:21 +0300

    fix: поправить работу инпутов

--- script/script.js
+++ script/script.js
@@ -1,29 +1,27 @@
-// Сценарий открытия/закрытия попапа
 const buttonEdit = document.querySelector('.profile__button_edit');
 const popup = document.querySelector('.popup');
 const buttonClose = document.querySelector('.popup__close-button');
 let nameInput = document.querySelector('#heading');
 let jobInput = document.querySelector('#subheading');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__form');
 
 buttonEdit.addEventListener('click', function() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 });
 
 function popupClose() {
   popup.classList.remove('popup_opened');
-}
-
+};
 buttonClose.addEventListener('click', popupClose);
 
-// Отправка данных в форме
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-
-let formElement = document.querySelector('.popup__form');
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
+    popupClose();
 };
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', popupClose);
