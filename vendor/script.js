77eccf9b71f3169207d2b780770b160cb43bec7f
Sergei Musailov <sergeymusailov@yandex.ru>
2020-05-24 16:00:05 +0300

    feat: добавить автоматически карточки из массива

--- vendor/script.js
+++ vendor/script.js
@@ -1,6 +1,11 @@
 const buttonEdit = document.querySelector('.profile__button_edit');
 const popup = document.querySelector('.popup');
 const buttonClose = document.querySelector('.popup__close-button');
+const buttonAddClose = document.querySelector('.popup-add__close-button');
+const popupAdd = document.querySelector('.popup-add');
+const buttonAdd = document.querySelector('.profile__button_add');
+const elementsContainer = document.querySelector('.elements');
+
 let nameInput = document.querySelector('#heading');
 let jobInput = document.querySelector('#subheading');
 let profileTitle = document.querySelector('.profile__title');
@@ -34,6 +39,7 @@ const initialCards = [
   }
 ];
 
+//попап редактирование
 buttonEdit.addEventListener('click', function() {
   popup.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
@@ -52,3 +58,23 @@ function formSubmitHandler (evt) {
     popupClose();
 };
 formElement.addEventListener('submit', formSubmitHandler);
+
+//попап добавление фотографии
+buttonAdd.addEventListener('click', function() {
+  popupAdd.classList.add('popup-add_opened');
+});
+
+function popupAddClose() {
+  popupAdd.classList.remove('popup-add_opened');
+};
+buttonAddClose.addEventListener('click', popupAddClose);
+
+//добавление карточек
+for (let i = 0; i < initialCards.length; i++) {
+  const elementTemplate = document.querySelector('#element-template').content;
+  const element = elementTemplate.cloneNode(true);
+
+  element.querySelector('.element__cover').src = initialCards[i].link;
+  element.querySelector('.element__title').textContent = initialCards[i].name;
+  elementsContainer.append(element);
+};
